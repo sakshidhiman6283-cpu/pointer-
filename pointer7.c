70bf1e9652f95402c103126ecfd8bfc90b1f096b
sakshidhiman6283-cpu <sakshidhiman6283@gmail.com>
2026-07-12 23:25:38 -0700

    Add files via upload

---
+++ pointer7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int *ptr = NULL;
+
+    if (ptr == NULL)
+    {
+        printf("pointer is NULL");
+    }
+    else{
+        printf("Pointer is not NULL");
+    }
+
+    return 0;
+}
